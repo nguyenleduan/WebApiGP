c5a8fc743e6cd485f6d3725d2ae31cde5059680e
duannguyen <DuanNguyen@usexpress.vn>
2022-04-05 15:38:38 +0700

    upload

---
+++ WebApiGP/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiGP.Models
+{
+    public class User: InfoUSer
+    {
+        public Guid Id { get; set; } 
+    }
+    public class InfoUSer
+    {
+        public String Name { get; set; }
+        public String Address { get; set; }
+        public double Count { get; set; }
+
+    }
+}
